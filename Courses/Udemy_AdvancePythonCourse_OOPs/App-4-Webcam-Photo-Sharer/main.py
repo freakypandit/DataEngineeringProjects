4c6377f312a9001cb6a943c3994280153e794b91
Deepak Pandey <40865954+freakypandit@users.noreply.github.com>
2024-01-26 07:08:52 +0000

    code files added for App 5

--- Courses/Udemy_AdvancePythonCourse_OOPs/App-4-Webcam-Photo-Sharer/main.py
+++ Courses/Udemy_AdvancePythonCourse_OOPs/App-4-Webcam-Photo-Sharer/main.py
@@ -1,24 +1,43 @@
 from kivy.app import App 
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder 
+import time 
 
 Builder.load_file('cameraCaptureApp.kv')
 
-from filesharer import FileSharer 
 
 class CameraScreen(Screen):
    
    def start(self):
       self.ids.camera.play = True
+      self.ids.camera_button.text = 'Stop Camera'
+      self.ids.camera.texture = self.ids.camera._camera.texture
 
    def stop(self):
       self.ids.camera.play = False
+      self.ids.camera_button.text = 'Start Camera'
+      self.ids.camera.texture = None
+   
 
    def capture(self):
-      pass
+      current_time = time.strftime('%Y%m%d-%H%M%S')
+      filepath = f"files/{current_time}.png"
+      self.ids.camera.export_to_png(filepath)
+
+      self.manager.current = 'image_screen'
+      self.manager.current_screen.ids.img_id.source = filepath
 
 class ImageScreen(Screen):
-   pass
+   
+   def createSharableLink(self):
+      pass 
+
+   def copy_link(self):
+      pass 
+
+   def open_link(self):
+      pass
+
 
 class RootWidget(ScreenManager):
    pass
